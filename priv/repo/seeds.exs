1688ba2628c8bceb2a5adc656ef78ced29645e51
Ivan Ernandorena <ivan.ernandorena@gmail.com>
2021-08-29 20:42:52 -0300

    Create customers in seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+alias Minig.Customer
+
+customers = [
+  %{first_name: "Ivan", last_name: "Ernandorena"},
+  %{first_name: "John", last_name: "Doe"},
+  %{first_name: "Jane", last_name: "Doe"}
+]
+
+customers
+|> Enum.map(fn c -> Customer.changeset(%Customer{}, c) end)
+|> Enum.map(&Minig.Repo.insert!(&1))
