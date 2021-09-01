9666ceef530ad3bb8e65c01e24493c9aec401f68
Ivan Ernandorena <ivan.ernandorena@gmail.com>
2021-09-01 15:49:20 -0300

    Remove unused file

--- utils/test.sh
+++
@@ -1,7 +0,0 @@
-curl -F 'image=@./26043984b91f3bfd718269e8df16676d.jpg' -F 'customer_id=1' -F 'description=A nice description' http://localhost:4000/minig/v1/post
-
-curl http://localhost:4000/minig/v1/post
-curl http://localhost:4000/minig/v1/post?page_number=1&page_size=10
-curl http://localhost:4000/minig/v1/post/1
-curl
-curl --request PATCH http://localhost:4000/minig/v1/post/1/1/like
